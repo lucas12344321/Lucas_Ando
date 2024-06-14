677cf8c726900a7b1e97fd97c31affb3389b970a
lucas12344321 <110060847+lucas12344321@users.noreply.github.com>
2024-06-14 09:15:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const botoes = document.querySelectorAll(".botao");
+const conteudo = document.querySelectorAll(".aba-conteudo")
+const tempo = document.querySelectorAll("contador")
+console.log(tempo);
+for(let i = 0; 1 < botoes.length ; i++){
+    botoes[i].onclick = function() {
+        for (j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            conteudo[j].classList.remove("ativo")
+        }
+        botoes[i].classList.add("ativo")
+        conteudo[i].classList.add("ativo")
+    }
+}

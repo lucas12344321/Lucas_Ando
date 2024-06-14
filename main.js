d60b79c70dc809c55c420c6b54ba3f2e364d4636
lucas12344321 <110060847+lucas12344321@users.noreply.github.com>
2024-06-14 09:43:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo")
 const tempo = document.querySelectorAll("contador")
+const tempoObjetivo1 = new Date(2024,6,25,4);
+tempo[1].textContent = agora
+const tempoObjetivo2 = new Date(2024,5,31);
+tempo[2].textContent = "olá"
+const tempoObjetivo3 = new Date(2024,6,20);
+tempo[3].textContent = "olá"
+const tempoObjetivo4 = new Date(2024,7,30);
+tempo[4].textContent = "olá"
+const agora = new Date();
 console.log(tempo);
 for(let i = 0; 1 < botoes.length ; i++){
     botoes[i].onclick = function() {
